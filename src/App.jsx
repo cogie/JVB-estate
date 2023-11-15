aa23bcdc7a8e4aff4ff6a473e95e08f9491bad67
Kojiee <mac7barcela@gmail.com>
2023-11-15 21:03:20 +0800

    Header Components

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import SignUp from './pages/Signup'
 import About from './pages/about'
+import Header from './components/Header'
 
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/sign-in' element={<SignIn />}/>
