0ef8f067ad8c67f3eb96d36e009390252c1a35e0
Kojiee <mac7barcela@gmail.com>
2023-11-22 18:17:37 +0800

    created sign up page functionality to be fixed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,17 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+  //Keep all the changes from the form and keep all data
+  const [formData, setFormData] = useState({}); 
+  const [error, setError] = useState(null); //handle the error
+  const [loading, setLoading] = useState(false); // set the loading after sign up
+
+  const handleChange = (e)=>{
+    setFormData({
+      //keep all information so data wont lose
+      ...formData,
+      [e.target.id]: e.target.value, 
+    });
+  };
+
+  // //handle the submitted data form the form/fetch
+  // const handleSubmit  = async (e)=>{
+  // e.preventDefault();
+  // setLoading(true);
+  // const res = await fetch('/api/auth/signup',
+  //   { //change form data into string using stringyfy
+  //     method:'POST',
+  //     headers:{
+  //       'Content-type': 'application/json',
+  //      },
+  //     body: JSON.stringify(formData),
+  //   });
+  //   //convert response to json
+  //   const data  = await res.json();
+  //   if(data.success === false){ //send an error
+  //     setError(data.message);
+  //     setLoading(false);
+  //     return;
+  //   }
+  //   setLoading(false);
+  // };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      // navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  // console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-
-      <form className='flex flex-col gap-4 '>
-        <input type="username" placeholder='username' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
+        <input type="username" placeholder='username' className='border p-3 rounded-lg' id='username'onChange={handleChange} />
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'onChange={handleChange} />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'onChange={handleChange} />
         {/* button add disable later on*/}
-        <button className='bg-slate-700 p-3 rounded-lg uppercase text-white hover:opacity-95 disabled:opacity-80 '>Sign Up</button>
+        <button disabled={loading} className='bg-slate-700 p-3 rounded-lg uppercase text-white hover:opacity-95 disabled:opacity-80 '>
+          {loading ?  'loading..': 'Sign Up'}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p className=''>Already have an account?</p>
@@ -19,6 +79,7 @@ export default function SignUp() {
           <span className='text-blue-700 '>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-600 mt-5'>{error}</p>}
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: "http://localhost:3000", //proxy, each time see the api add the locahost:3000 at the beggining
+        secure:false,
+      },
+    },
+  },
+  
   plugins: [react()],
-})
+});
