1aeb797c4438543abce38d10e470df337532835d
Kojiee <mac7barcela@gmail.com>
2023-12-04 21:27:01 +0800

    CreateListing UI done

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,9 +1,11 @@
-
+import React
+ from "react"
 export default function CreateListing() {
   return (
     <main className="p-3 max-w-4xl mx-auto">
     <h1 className="text-3xl font-semibold text-center my-7">Create a Listing</h1>
-    <form className="flex flex-col sm:flex-row ">
+    <form className="flex flex-col sm:flex-row gap-4">
+        {/* 1st div */}
         <div className="flex flex-col gap-4 flex-1">
             <input type="text" placeholder="Name" className="border p-3 rounded-lg" id="name" maxLength="62" minLength="10" required/>
             <textarea type="text" placeholder="Description" className="border p-3 rounded-lg" id="description" required/>
@@ -30,6 +32,44 @@ export default function CreateListing() {
                     <span>Offer</span>
                 </div>
             </div>
+            {/* 2nd div */}
+            <div className="flex gap-6 flex-wrap">
+                <div className="flex gap-2 items-center">
+                    <input className='p-3 border border-gray-300 rounded-lg' type="number" id='bedrooms' min='1' max='10' required/>
+                    <p>Beds</p>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <input className="p-3 border border-gray-300 rounded-lg" type="number" id='bathrooms' min='1' max='10'required/>
+                    <p>Bath</p>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <input className=" p-3 border border-gray-300 rounded-lg" type="number" id='regularPrice' min='1' max='10'required/>
+                    <div className="flex flex-col items-center">
+                        <p>Regular price</p>
+                        <span className="text-xs">(₱ / Month)</span>
+                    </div>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <input className="border border-gray-300 rounded-lg p-3" type="number" id='discountedPrice' min='1' max='10'required/>
+                    <div className="flex flex-col items-center">
+                        <p>Discounted price</p>
+                        <span className="text-xs">(₱ / Month)</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* 2nd div */}
+        <div className="flex flex-col flex-1 gap-4">
+            <p className="font-semibold">Images:
+                <span className="font-normal text-gray-600 ml-2">The first image will be the cover (max 6)</span> 
+            </p>
+            <div className="flex gap-4 ">
+                <input className="p-3 border border-gray-300 rounded w-full" type="file" id="images" accept="image/*" multiple/>
+                <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+                    Upload
+                </button>
+            </div>
+            <button className="p-3 rounded-lg bg-slate-700 text-white uppercase hover:opacity-95 disabled:opacity-80">Create Listing</button>
         </div>
     </form>
     </main>
