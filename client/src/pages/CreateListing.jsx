4679ae5b6e92616169c4421c1e2108ac7137522d
Kojiee <mac7barcela@gmail.com>
2023-12-05 21:24:25 +0800

    listing image upload/ error on upload button

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,55 @@
-import React
- from "react"
+import { useState } from "react"
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase'
+
 export default function CreateListing() {
+    const [files, setFiles] = useState([]);
+
+    const [formData, setFormData] = useState({
+        imageUrls: [],
+
+    });
+    console.log(formData);
+    //image
+    const handleImageSubmit =  (e) => {
+        if(files.length > 0 && files < 7){
+            const promises = []; //return multiple images
+
+            //function for each images upload/storing
+            for (let i = 0; i < files.length; i++){
+                promises.push(storeImage(files[i]));
+            }
+            Promise.all(promises).then((urls) => {
+                setFormData({...formData, imageUrls: formData.imageUrls.concat(urls), 
+                });
+            });
+        }
+    };
+
+    //handles urls that will be stored in promises
+    const storeImage = async (file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file); 
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done`)
+                },
+                (error)=> {
+                    reject(error);
+                },
+                ()=> {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                    });
+                }
+            );   
+        });
+    };
   return (
     <main className="p-3 max-w-4xl mx-auto">
     <h1 className="text-3xl font-semibold text-center my-7">Create a Listing</h1>
@@ -64,8 +113,10 @@ export default function CreateListing() {
                 <span className="font-normal text-gray-600 ml-2">The first image will be the cover (max 6)</span> 
             </p>
             <div className="flex gap-4 ">
-                <input className="p-3 border border-gray-300 rounded w-full" type="file" id="images" accept="image/*" multiple/>
-                <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+                <input onChange={(e) => setFiles(e.target.files)} className="p-3 border border-gray-300 rounded w-full" type="file" id="images" accept="image/*" multiple/>
+                <button type="button" 
+                onClick={handleImageSubmit} 
+                className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
                     Upload
                 </button>
             </div>
