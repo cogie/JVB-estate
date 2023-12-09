56411a0d4cbbbd78577f756937b7ee73976e5c25
Kojiee <mac7barcela@gmail.com>
2023-12-09 21:38:34 +0800

    complete delete listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,10 +1,32 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 //create a listing from the user
 export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
+    } catch (error) {
+        next(error);
+    }
+};
+
+//delete listing
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    //if there is no listing
+    if(!listing){
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    //if user id is not valid
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, 'You can only delete your own listings!'));
+    }
+
+    //if everything is good del listing
+    try {
+         await Listing.findByIdAndDelete(req.params.id); 
+         res.status(200).json('Lising has been deleted!');
     } catch (error) {
         next(error);
     }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -150,6 +150,26 @@ export default function Profile() {
     }
   };
 
+  //delete user lisitings
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json(); //convert to json
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      //if success get prev data then filter out
+      setUserListings((prev) => 
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto ">
       <h1 className='text-center my-7 text-3xl font-semibold'>Profile</h1>
@@ -225,8 +245,15 @@ export default function Profile() {
             </Link>
 
             <div className="flex flex-col items-center">
-              <button className="text-red-700 uppercase hover:shadow-lg">Delete</button>
-              <button className="text-green-700 uppercase hover:shadow-lg">Edit</button>
+              <button 
+              onClick={()=> handleListingDelete(listing._id)}
+              className="text-red-700 uppercase hover:shadow-lg">
+                Delete
+              </button>
+              <button 
+              className="text-green-700 uppercase hover:shadow-lg">
+                Edit
+              </button>
             </div>
           </div>
         ))}
