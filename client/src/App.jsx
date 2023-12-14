173ccf04190fb853f53733b3d93755c213df4318
Kojiee <mac7barcela@gmail.com>
2023-12-14 19:10:57 +0800

    set up listing page and Swiper package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/updateListing';
+import Listing from './pages/Listing';
 
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
           <Route path='/sign-in' element={<SignIn />}/>
           <Route path='/sign-up' element={<SignUp />}/>
           <Route path='/about' element={<About />}/>
+          <Route path='/listing/:listingId' element={<Listing />}/>
 
           <Route element = {<PrivateRoute/>}> 
           {/* make profile priveate */}
---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react'; 
+import  SwiperCore  from "swiper";
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+
+export default function Listing() {
+    const params = useParams();
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    SwiperCore.use([Navigation]);
+    
+    useEffect(() => {
+        const fetchListing = async () => {
+            try {
+                setLoading(true);
+                const res = await fetch(`/api/listing/get/${params.listingId}`)
+                const data = await res.json();
+                //if success false
+                if (data.success === false) {
+                    setError(true);
+                    setLoading(false);  
+                    return;
+                }
+                setListing(data); // if success === true
+                setLoading(false);
+                setError(false);
+            } catch (error) {
+                setError(true);
+                setLoading(false);
+            }
+        };
+        fetchListing();
+    }, [params.listingId]);
+    console.log(loading)
+  return (
+    <main>
+        {/* loading effect end error */}
+        {loading && <p className="text-center my-7 text-2xl">Loading...</p>} 
+        {error && <p className="text-center my-7 text-2xl">Something went wrong!</p>}  
+
+         {/*show img at the top  */}
+         {listing && !loading && !error && (
+                <h1>{listing.name}</h1>
+            )
+         }
+    </main>
+  )
+}
