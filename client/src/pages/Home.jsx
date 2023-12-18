9dd5e68aa15285586613d1c1351b48387f81ce5b
Kojiee <mac7barcela@gmail.com>
2023-12-18 21:41:24 +0800

    added a comments on what functionality needed this week

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+
+        {/* 8 functions need to be done this week
+          Create search API route -
+
+          Complete header search form functionality -
+
+          Create search page Ul -
+
+          Add onChange and onSubmit functionality to the search page -
+
+          Create the listing item component and show listings -
+
+          Add show more listings functionality -
+
+          Complete home page -
+
+          Complete about page -
+        */}
+
+
+    </div>
   )
 }
