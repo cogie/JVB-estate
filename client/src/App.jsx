f2f6bfb3e3c97457a72162bf6fbb784956e39b3a
Kojiee <mac7barcela@gmail.com>
2024-01-03 14:55:09 +0800

    resolve the ./page/About

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
-import About from './pages/about';
+import About from './pages/About';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
