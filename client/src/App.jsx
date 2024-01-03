bdb90154eec9f5cf729d552eca7552915597c64f
Kojiee <mac7barcela@gmail.com>
2024-01-03 14:59:34 +0800

    resolve app.sx for UpdateLisitng

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import About from './pages/About';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
-import UpdateListing from './pages/updateListing';
+import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
 
